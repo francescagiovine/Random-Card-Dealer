2b4ef9c6d69621432cbedbe507e7c1a7b013a489
francescagiovine <francescagiovine@hotmail.com>
2022-02-04 17:37:56 +0000

    first init

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".top").innerHTML = "&spades;";
+  document.querySelector(".number").innerHTML = number[cardNumber];
+  document.querySelector(".bottom").innerHTML = "&spades;";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let type = ["♥", "♣", "♠", "♦"];
+
+  let cardNumber = Math.floor(Math.random() * number.length);
+  let cardType = Math.floor(Math.random() * type.length);
 };
+
+//window.onload = function cardBuilder() {
+//write your code here
+
+// let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+//let symbols = ["♦", "♥", "♠", "♣"];
+// let number = Math.floor(Math.random() * numbers.length);
+
+// document.querySelector(".number").innerHTML = numbers[number]; //elemento del array aleatorio
+
+// let symbol = Math.floor(Math.random() * symbols.length);
+
+// document.querySelector(".icon-left").innerHTML = symbols[symbol];
+// document.querySelector(".icon-right").innerHTML = symbols[symbol];
+//};
